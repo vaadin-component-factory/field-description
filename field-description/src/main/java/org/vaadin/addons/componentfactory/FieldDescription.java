99d6e15e00845811d580768a15ed1cd53cc9e5e5
Unknown <unknown@example.com>
2021-08-18 13:00:19 +0100

    Added - Project set up

---
+++ field-description/src/main/java/org/vaadin/addons/componentfactory/FieldDescription.java
@@ -0,0 +1,11 @@
+package org.vaadin.addons.componentfactory;
+
+import com.vaadin.flow.component.dependency.NpmPackage;
+
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
+public class FieldDescription {
+    public FieldDescription() {
+        //TODO.
+    }
+}
